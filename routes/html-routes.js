158cd8efa23f6c31685aa27b03c1136304022493
risaco <rhia.cooper@gmail.com>
2017-10-11 19:49:03 -0500

    added html route for home

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+// ***** DEPENDENCIES *****
+var path = require("path");
+
+// ***** ROUTING *****
+module.exports = function(app){
+
+	// -- Home --
+	app.get("/", function(req, res) {
+
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	});
+}
