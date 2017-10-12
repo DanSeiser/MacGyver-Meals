55dcd8e8f92d1be947b71dc4d7514ff5bce8206f
risaco <rhia.cooper@gmail.com>
2017-10-11 20:39:53 -0500

    api-routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,14 @@
-// ***** LOAD DATA *****
-var recipeData = require("../data/recipeData");
+// ***** DEPENDENCIES *****
+var db = require("../models");
 
 // ***** ROUTING *****
 
 module.exports = function(app) {
 
 	// previous searches
-	app.get("api/previous", function(req, res) {
-		res.json(recipeData);
+	app.get("/api/previous", function(req, res) {
+		db.Query.findAll({}).then(function(dbQuery){
+			res.json(dbQuery);
+		});
 	});
 }
--- server.js
+++ server.js
@@ -27,10 +27,11 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js");
+require("./routes/api-routes.js");
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
